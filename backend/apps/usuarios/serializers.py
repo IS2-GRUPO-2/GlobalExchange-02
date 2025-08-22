fbdea0e5e3d4989b9f7bcc3225dfd580c48e6af4
EJFR009 <ejfr09@fpuna.edu.py>
2025-08-22 06:41:31 -0300

    crud de usuarios v3

--- backend/apps/usuarios/serializers.py
+++ backend/apps/usuarios/serializers.py
@@ -4,6 +4,6 @@ from .models import Usuario
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['idUsuario', 'username', 'email', 'telefono', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined']
+        fields = ['idUsuario', 'username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined']
 
 
