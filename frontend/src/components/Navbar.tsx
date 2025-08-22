e70d8fb679a98bfab1af2b018e33b001a9f42cd0
jose-ramirez-03 <joseramirezdure03@gmail.com>
2025-08-22 01:09:17 -0300

    IS2-94 * roles en el navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,6 +14,7 @@ const navigation = [
   { name: "Team", href: "#", current: false },
   { name: "Projects", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
+  { name: "Roles", href: "/roles", current: false }, 
 ];
 
 interface NavigationItem {
