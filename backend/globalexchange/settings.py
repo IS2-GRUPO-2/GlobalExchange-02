0acbe8e7403fb30aa48ab2186bf9cf6d463e9090
jose-ramirez-03 <joseramirezdure03@gmail.com>
2025-08-22 16:47:33 -0300

    IS2-94 * HOTFIX

--- backend/globalexchange/settings.py
+++ backend/globalexchange/settings.py
@@ -140,6 +140,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Swagger: desactivar renderers en modo compatibilidad
+SWAGGER_USE_COMPAT_RENDERERS = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
