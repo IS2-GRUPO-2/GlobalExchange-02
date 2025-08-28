7de78d23a8079035707335a3e7a15570376ba2be
jose-ramirez-03 <joseramirezdure03@gmail.com>
2025-08-27 21:34:28 -0300

    IS2-101 * Filters para roles

--- backend/apps/roles/views.py
+++ backend/apps/roles/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from django.contrib.auth.models import Group
 from .serializers import RoleSerializer
 
@@ -6,4 +6,6 @@ class RoleViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().prefetch_related("permissions")
     serializer_class = RoleSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id', 'name', 'permissions__name']
 
