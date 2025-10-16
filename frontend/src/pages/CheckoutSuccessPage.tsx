de1c94286a8094b71c2968085e7ebee66dba089a
Chellus <elias4845@gmail.com>
2025-10-16 14:39:03 -0300

    IS2-26 * Removidas librerias no usadas

--- frontend/src/pages/CheckoutSuccessPage.tsx
+++ frontend/src/pages/CheckoutSuccessPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
