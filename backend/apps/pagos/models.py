54efcc6bc9ca04c3186b71a66b83b7e1adaeb558
Chellus <elias4845@gmail.com>
2025-10-22 21:45:32 -0300

    IS2-189 * Agregados campos al modelo de pagos

--- backend/apps/pagos/models.py
+++ backend/apps/pagos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from apps.metodos_financieros.models import MetodoFinanciero
-
+from apps.operaciones.models import Transaccion
 
 # Create your models here.
 class Pagos(models.Model):
@@ -9,9 +9,10 @@ class Pagos(models.Model):
         ("APROBADO", "Aprobado"),
         ("FALLIDO", "Fallido"),
     ]
-
+    transaccion = models.OneToOneField(Transaccion, on_delete=models.PROTECT)
     metodo_pago = models.ForeignKey(MetodoFinanciero, on_delete=models.PROTECT)
     request = models.CharField(max_length=255, null=True, blank=True)
+    stripe_checkout_session_id = models.CharField(max_length=100, null=True, blank=True)
     response = models.CharField(max_length=255, null=True, blank=True)
     estado = models.CharField()
     created_at = models.DateTimeField(auto_now_add=True)
