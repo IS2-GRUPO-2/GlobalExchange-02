e59b122928a695aeef31e02e9d952c0a89ee456b
JavierGoto02 <gotojavier9@gmail.com>
2025-10-23 16:16:31 -0300

    IS2-191 * Modificacion de formula de tasa de cambios

--- backend/apps/cotizaciones/service.py
+++ backend/apps/cotizaciones/service.py
@@ -4,13 +4,15 @@ from decimal import Decimal, ROUND_DOWN
 from apps.divisas.models import Divisa
 from apps.operaciones.models import MetodoFinanciero
 from apps.clientes.models import Cliente
+
+
 class TasaService:
     """
     Servicio central de cálculo de tasas de cambio.
     Contiene métodos estáticos para calcular tasas en distintos contextos:
     - Venta / Compra
     """
-    
+
     @staticmethod
     def crear_historial(tasa: Tasa) -> HistorialTasa:
         return HistorialTasa.objects.create(
@@ -19,47 +21,43 @@ class TasaService:
             tasaVenta=TasaService.calcular_tasa_venta(tasa),
 
         )
-   
+
     @staticmethod
-    def calcular_tasa_compra(tasa: Tasa, com_metodo: Decimal=None, cliente: Cliente=None) -> Decimal:
+    def calcular_tasa_compra(tasa: Tasa, com_metodo: Decimal = None, cliente: Cliente = None) -> Decimal:
         """
         Calcula la tasa de compra final
         """
-        precio_base = Decimal(tasa.precioBase)
-        comision_base = Decimal(tasa.comisionBaseCompra)
+        precio_base = tasa.precioBase
+        comision_base = tasa.comisionBaseCompra
+
+        tasa_compra = precio_base - comision_base
+
         if cliente:
-            categ_descuento = Decimal(cliente.id_categoria.descuento)
-        else:
-            categ_descuento = Decimal(0)
+            categ_descuento = cliente.id_categoria.descuento / Decimal('100')
+            tasa_compra = tasa_compra + tasa_compra * categ_descuento
 
         if com_metodo:
-            com_metodo = Decimal(com_metodo)
-        else:
-            com_metodo = Decimal(0)
+            com_metodo = com_metodo / Decimal('100')
+            tasa_compra = tasa_compra - tasa_compra * com_metodo
 
-        return (
-            precio_base * (1 - com_metodo / 100) - comision_base * (1 - categ_descuento / 100)
-        )
-    
+        return tasa_compra
+
     @staticmethod
-    def calcular_tasa_venta(tasa: Tasa, com_metodo: Decimal=None, cliente: Cliente=None) -> Decimal:
+    def calcular_tasa_venta(tasa: Tasa, com_metodo: Decimal = None, cliente: Cliente = None) -> Decimal:
         """
         Calcula la tasa de venta final
         """
-        precio_base = Decimal(tasa.precioBase)
-        comision_base = Decimal(tasa.comisionBaseVenta)
+        precio_base = tasa.precioBase
+        comision_base = tasa.comisionBaseVenta
+
+        tasa_venta = precio_base + comision_base
 
         if cliente:
-            categ_descuento = Decimal(cliente.id_categoria.descuento)
-        else:
-            categ_descuento = Decimal(0)
+            categ_descuento = cliente.id_categoria.descuento / Decimal('100')
+            tasa_venta = tasa_venta - tasa_venta * categ_descuento
 
         if com_metodo:
-            com_metodo = Decimal(com_metodo)
-        else:
-            com_metodo = Decimal(0)
+            com_metodo = com_metodo / Decimal('100')
+            tasa_venta = tasa_venta + tasa_venta * com_metodo
 
-        return (
-            precio_base * (1 + com_metodo / 100) + comision_base * (1 - categ_descuento / 100)
-        )
-
+        return tasa_venta
